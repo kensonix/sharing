fa02e975d29393233b82f2c757581439c73c751e
Administrator <Administrator@172.20.61.144>
2018-11-26 19:03:42 +0800

    代码提交

--- src/fruits/Fruit.java
+++ src/fruits/Fruit.java
@@ -20,4 +20,20 @@ public abstract class Fruit {
 	public String toString(){
 		return "Fruit's name : " + this.name + "\r\n";
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
