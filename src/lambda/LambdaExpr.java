54a9ff16405fe11cc7248f36b8c594cdc15ead40
Administrator <Administrator@172.20.61.144>
2018-11-26 19:13:27 +0800

    提交

--- src/lambda/LambdaExpr.java
+++ src/lambda/LambdaExpr.java
@@ -61,37 +61,4 @@ public class LambdaExpr {
 		Thread thread2  = new Thread( () -> System.out.println("thread2 created") );
 	}
 	
-	@Test
-	public void test4(){
-		 final long count = 10000000;
-	        List<Long> list = new ArrayList<>();
-	        for (long i = 0; i < count; i++) {
-	            list.add(i);
-	        }
-
-	        //=========传统方式进行外部迭代=========
-	        Instant begin = Instant.now();
-	        for (Long i : list) {
-	            System.out.print("");
-	        }
-	        System.out.println("--------------------------");
-	        Instant end = Instant.now();
-	        System.out.println("传统方式进行外部迭代" + count + "次,耗时(ms)：" + Duration.between(begin, end).toMillis());
-
-
-	        //=========java8内部迭代，用lambda处理=========
-	        begin = Instant.now();
-	        list.stream().forEach(i -> System.out.print(""));
-	        System.out.println("--------------------------");
-	        end = Instant.now();
-	        System.out.println("内部迭代forEach" + count + "次,耗时(ms)：" + Duration.between(begin, end).toMillis());
-
-	        //=========java8进行并行流处理后迭代（备注：并行流输出是没有顺序的 比如不再是1234顺序了）=========
-	        begin = Instant.now();
-	        list.parallelStream().forEach(i -> System.out.print(""));
-	        System.out.println("--------------------------");
-	        end = Instant.now();
-	        System.out.println("内部迭代parallelStream" + count + "次,耗时(ms)：" + Duration.between(begin, end).toMillis());
-	    }
-	
 }
